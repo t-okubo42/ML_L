cc0f8c9b350c037e7746fbffca0fbc9afd84fec8
Toshiki Okubo <t-okubo@s.otpe.k.u-tokyo.ac.jp>
2020-06-03 16:29:40 +0900

    取り敢えず解析はできる
    精度は低い

--- titanic.py
+++ titanic.py
@@ -7,21 +7,33 @@ from sklearn.metrics import accuracy_score
 
 
 
-titanic = pd.read_csv('hogehoge.csv')
+titanic = pd.read_csv('hoge_text.csv')
+titanic_assignment = pd.read_csv('hoge_hoge_piyo.csv')
 
-y = titanic['Piyopiyo']
-x = np.array(titanic.drop(['Piyopiyo', 'Mumyamumya'], axis=1))
+y = titanic['Survived']
+x = np.array(titanic.drop(['Survived', 'PassengerId'], axis=1))
 
-y_a = np.array(y)
-x_a = np.array(x)
+y_array = np.array(y)
+x_array = np.array(x)
 
-x_train, x_test, y_train, y_test = train_test_split(x_a, y_a, test_size = 0.4, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x_array, y_array, test_size = 0.251, random_state = 0)
 
 rfc = RandomForestClassifier(random_state = 0)
 rfc.fit(x_train, y_train)
 
-y_pred = rfc.predict(x_test)
 
 
+x1 = np.array(hoge_hoge.drop(['PassengerId'], axis=1))
+x1_test = np.array(x1)
+
+
+y_pred = rfc.predict(x1_test)
+
+print(x1_test.shape)
+print(y_pred.shape)
 print(f'{accuracy_score(y_pred, y_test):5f}')
-print('x_train', x_train.shape)
+
+
+PassengerId = np.array(titanic_assignment["PassengerId"]).astype(int)# PassengerIdを取得
+y_sol = pd.DataFrame(y_pred, PassengerId, columns = ["Survived"])# y_predとPassengerIdをデータフレームに
+y_sol.to_csv("hoge_test.csv", index_label = ["PassengerId"])# hogehoge.csvとして書き出し
